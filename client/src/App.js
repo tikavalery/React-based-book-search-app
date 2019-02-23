d62b8180e52d3f25be5dd975f0517de00aa39e23
tikavalery <43121065+tikavalery@users.noreply.github.com>
2019-02-23 01:36:01 -0800

    chnages to index and app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     (React) Google Books
+  
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li>
+          
+              Search
+         
+          </li>
+          <li >
+            
+              Saved
+          
+          </li>
+        </ul>
+      </div>
+    </nav>
       </div>
     );
   }
